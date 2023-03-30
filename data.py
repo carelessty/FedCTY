a054ca8517d01415af8c74996d1944d06f9746a2
chen <294893644@protonmail.com>
2023-03-30 14:19:10 +0800

    test

--- data.py
+++ data.py
@@ -142,7 +142,7 @@ class CIFARDataset(Dataset):
 
         if k is None:
             self.X = dataset[0][1]
-            yt = np.concatenate(dataset[1][k])
+            yt = np.array(dataset[1][k])
             self.y = torch.from_numpy(yt).type(torch.long)
         else:
             self.X = dataset[0][k]
